052c21c53f9f830cf0b84c029c5e684a80f34bfb
Eli <e.m.mills1995@gmail.com>
2021-11-21 14:22:02 -0500

    Created initial template.

---
+++ HasamiShogiGame.py
@@ -0,0 +1,33 @@
+# Author: Eli Mills
+# Date: 11/21/2021
+# Description: Portfolio Project - Hasami Shogi
+
+class HasamiShogiGame:
+    """Defines the methods for playing a game of Hasami Shogi."""
+    def __init__(self):
+        """Required."""
+        pass
+
+    def get_game_state(self):
+        """Required."""
+        pass
+
+    def get_active_player(self):
+        """Required."""
+        pass
+
+    def get_num_captured_pieces(self, player):
+        """Required."""
+        pass
+
+    def make_move(self, moved_from, moving_to):
+        """Required."""
+        pass
+
+    def get_square_occupant(self, square):
+        """Required."""
+        pass
+
+
+
+
